a653a739dbc976e9dcf57596d161647484c54ee2
R.K.Subash <subashkohila24092007@gmail.com>
2025-10-17 14:17:07 +0530

    little corrections

--- temp_gradient.py
+++ temp_gradient.py
@@ -6,19 +6,13 @@ import numpy
 temp_gradients_x = []
 temp_gradients_y = []
 
-old_temp_gradients_x = []
-old_temp_gradients_y = []
-
 with open("./temp_gradient.csv", "r") as temp_g_f:
   reader = csv.reader(temp_g_f, skipinitialspace=True)
 
   for row in reader:
     temp_gradients_x.append(float(row[0]))
     temp_gradients_y.append(float(row[1]))
-
-old_temp_gradients_x = temp_gradients_x
-old_temp_gradients_y = temp_gradients_y
-
+    
 spline = make_interp_spline(temp_gradients_x, temp_gradients_y, k = 2) # returns a function which gives the interpolated values for the array passed
 temp_gradients_x = numpy.linspace(temp_gradients_x[0], temp_gradients_x[-1], endpoint=True)
 temp_gradients_y = spline(temp_gradients_x)
